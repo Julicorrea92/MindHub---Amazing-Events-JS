78f71c87245eb487d52e3aded3b6136cd84bdfde
Julian Correa <julicorrea92@gmail.com>
2023-02-22 21:18:12 -0300

    Avance ejercicio condicionales

---
+++ ejerciciosIntro2.js
@@ -0,0 +1,64 @@
+//En ejercicios de condicional se usan variables var para 
+//tener el mismo prompt para todos los casos
+
+var numero1 = prompt('Ingrese el primer número');
+var numero2 = prompt('Ingrese el segundo número');
+console.log(numero1);
+console.log(numero2);
+
+if (numero1>numero2){
+    console.log('El numero 1 es mayor que el numero 2');
+}
+else{
+    console.log('El numero 2 es mayor que el numero 1');
+}
+
+console.log('\n');
+
+
+var numero1 = prompt('Ingrese el primer número');
+var numero2 = prompt('Ingrese el segundo número');
+console.log(numero1);
+console.log(numero2);
+
+if (numero1 == numero2){
+    console.log('El numero 1 es IGUAL al numero 2');
+}
+else{
+    console.log('El numero 2 NO ES IGUAL al numero 1');
+}
+console.log('\n');
+
+var numero1 = prompt('Ingrese el primer número');
+var numero2 = prompt('Ingrese el segundo número');
+console.log(numero1);
+console.log(numero2);
+
+if (numero1 == numero2){
+    console.log('El numero 1 es IGUAL al numero 2');
+}
+else if (numero1 < numero2){
+    console.log('El numero 2 es mayor que el numero 1');
+}
+else 
+    console.log('El numero 1 es mayor que el numero 2');
+console.log('\n');
+
+
+var numero1 = prompt('Ingrese el primer número');
+var numero2 = prompt('Ingrese el segundo número');
+console.log(numero1);
+console.log(numero2);
+
+if (numero1 == numero2){
+    console.log('El numero 1 es IGUAL al numero 2');
+}
+else if (numero1 < numero2){
+    console.log('El numero 2 es mayor que el numero 1');
+}
+else 
+    console.log('El numero 1 es mayor que el numero 2');
+console.log('\n');
+
+
+
