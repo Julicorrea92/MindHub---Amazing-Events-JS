64c22cd8e3b151f7866ff4ed8546e33a66966a47
Julian Correa <julicorrea92@gmail.com>
2023-03-05 01:55:43 -0300

    1ra subida: Primeras aproximaciones a funciones de filtrado.

--- JS/main.js
+++ JS/main.js
@@ -1,18 +1,119 @@
-//Función comun
+//Función html de tarjeta dinámica
 
-function traerTarjeta(event){
+function traerTarjeta(event) {
 
     return `<div class="card my-3 py-2" style="width: 20rem;">
-                            <img src="${event.image}" class="" alt="...">
-                            <div class="card-body d-flex flex-column">
-                                <h5 class="card-title">${event.name}</h5>
-                                <p class="card-text">${event.description}</p>
-                                <div class="mt-auto">
-                                    <div class="d-flex justify-content-between">
-                                        <p class="parrafo">${event.price}</p>
-                                        <a href="Details.html" class="btn btn-danger">Ver Más</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>`;
+                    <img src="${event.image}" class="" alt="...">
+                <div class="card-body d-flex flex-column">
+                    <h5 class="card-title">${event.name}</h5>
+                    <p class="card-text">${event.description}</p>
+                    <div class="mt-auto">
+                        <div class="d-flex justify-content-between">
+                            <p class="parrafo">${event.price}</p>
+                            <a href="Details.html" class="btn btn-danger">Ver Más</a>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+}
+
+//Función html para filtrado de checkbox
+
+function filtradoCheck(event){
+
+    return `<div class="form-check form-check-inline">
+                <input class="form-check-input" type="checkbox" id="inlineCheckbox4" value="${event.category}">
+                <label class="form-check-label" for="inlineCheckbox4">${event.category}</label>
+            </div>`;
+}
+
+//Función html para search params
+
+function encontrarDetail(event) {
+    return `<div class="card" style="width: 20rem; height: 17rem; border: 1px solid;">
+                <img src="#" alt="">
+            </div>
+            <div class="card" style="width: 20rem; border: 1px solid">
+                <div class="card-body">
+                <h4 class="card-title">Título</h4>
+                <h6 class="card-title mb-2 text-muted">Texto Descriptivo</h6>
+                <h6 class="card-title mb-2 text-muted">Texto Descriptivo</h6>
+                <h6 class="card-title mb-2 text-muted">Texto Descriptivo</h6>
+                </div>
+            </div>
+            </div>`
+
+}
+
+//Pasar search y procesarlo
+
+let search = document.querySelector('form');
+search.addEventListener('submit', e => {
+    e.preventDefault();
+    /* console.log('El evento ingresado se capturó');
+    // Hasta acá registra el evento */
+    
+    let texto = document.querySelector('.form-control').value;
+    /* console.log(texto.toString().trim().toLowerCase());
+    //Acá pasa texto a string, le quita espacios y deja minúsculas
+    */    
+    texto.toString().trim().toLowerCase(); 
+
+    let dato = data.events;
+    
+
+
+
+
+    tarjeta.innerHTML = "";
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* function filtrar(texto){
+    for (let event of data.events) {
+        
+        if (event.name == texto || event.description == texto) {
+            console.log(event);
+            traerTarjeta(event);
+        }
+        else
+            alert('No se encuentran resultados. Modifique los filtros');
     }
+
+
+} */
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # MindHub---Amazing-Events-JS
 Repositorio donde van a encontrar las prácticas de Javascript del Bootcamp de MindHub en Argentina Programa 4.0
 
-En la rama Task 2 quedarán registrados los procesos de manejo de eventos de DOM.
+En la rama Task 3 quedarán registrados los procesos de manejo de eventos de DOM referidos a los filtros de busqueda y checkbox.
