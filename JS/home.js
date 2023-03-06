8251ee265dd304156e24f7636e2bc86a330a1a29
Julian Correa <julicorrea92@gmail.com>
2023-03-06 01:25:41 -0300

    3er. envío: Quedaron casi listos los filtros de búsqueda por texto. Corregir el estado inicial

--- JS/home.js
+++ JS/home.js
@@ -3,8 +3,7 @@
 let todasLasTarjetas = "";
 
 for (let event of data.events) {
-
-    todasLasTarjetas += traerTarjeta(event);
+todasLasTarjetas += traerTarjeta(event);
 }
 
 let tarjeta = document.getElementById('tarjeta');
@@ -15,57 +14,95 @@ tarjeta.innerHTML = todasLasTarjetas;
 
 let search = document.querySelector('form');
 search.addEventListener('submit', e => {
-    
-    e.preventDefault();
-    let texto = document.querySelector('.form-control').value;
-    console.log(texto.trim().toLowerCase());
-    //Acá toma el string le quita espacios y deja minúsculas
+//Si uso el event keyup no necesitaría el botón de búsqueda, aunque el boton queda obsoleto. Podría usarse como reset(?) 
+//Queda el submit que aplica mejor
 
-    let stringTexto = texto.trim().toLowerCase();
-
-    /*  let cajaTexto = [];
-    Probar si va array vacío
-    */
-    
-    let dato = data.events;
-    let traerNombre = dato.filter(tipo => tipo.name.toLowerCase().includes(stringTexto));
-    let traerDescripcion = dato.filter(tipo1 => tipo1.description.toLowerCase().includes(stringTexto));
-
-    console.log(traerNombre);
-    console.log(traerDescripcion);
-    //Acá si pongo algo que coincida me levanta el o los objetos. Poniento enjoy por ejemplo sale korean style y collectivities party
-})
+e.preventDefault();
+let texto = document.querySelector('.form-control').value.trim().toLowerCase();
+console.log('Texto capturado: '+ texto);
+//Se captura el evento sin problemas y ya queda sin espacios y en minuscula
 
 
 
 
 
+let filtrados = data.events.filter(e => {
+    return e.name.toLowerCase().includes(texto) || e.description.toLowerCase().includes(texto);
+});
+//Acá pude guardar en una variable más de un filtro
 
 
+let mostrarFiltrados = '';
 
- /*    if (traerNombre == stringTexto) {
-        document.querySelector('#tarjeta').innerHTML = traerTarjeta();
+if (filtrados.length > 0) {    
+    for (let filtro of filtrados) {
+        mostrarFiltrados += traerTarjeta(filtro);
     }
-    else if (traerDescripcion == stringTexto) {
-        document.querySelector('#tarjeta').innerHTML = traerTarjeta();
-    }
-    else {
-        alert('The search returned no results, please try again.')
-    }
-
-    Revisar porque siempre da falso, ver si estan cruzados los métodos de array con los de string
+    document.getElementById('tarjeta').innerHTML = mostrarFiltrados;
+} else {
+    alert('The search returned no results, please try again.');
+}
+});
+//Todo concentrado en un solo condicional
 
 
 
 
-       if (traerNombre.length > 0 || traerDescripcion.length > 0) {
-        document.querySelector('#tarjeta').innerHTML = traerTarjeta();
-    } else {
-        alert('The search returned no results, please try again.');
-    }
-
- */
 
 
+
+
+
+
+/*let stringTexto = texto.trim().toLowerCase();
+let traerNombre = data.events.filter(tipo => tipo.name.toLowerCase().includes(stringTexto));
+let traerDescripcion = data.events.filter(tipo => tipo.description.toLowerCase().includes(stringTexto));
+//Estas dos variables son arrays de objetos filtrados que se generan si stringTexto pasa el filtro */
+
+
+/* let mostrarNombre = "";
+
+for (let filtro of traerNombre) {
+mostrarNombre += traerTarjeta(filtro);
+}
+if (traerNombre.length > 0) {
+document.getElementById('tarjeta').innerHTML = mostrarNombre;
+}
+else {alert('The search returned no results, please try again.')
+}
+
+
+let mostrarDescripcion = "";
+
+for (let filtro of traerDescripcion) {
+mostrarDescripcion += traerTarjeta(filtro);
+}
+
+if (traerDescripcion.length > 0) {
+document.getElementById('tarjeta').innerHTML = mostrarDescripcion;
+}
+else {alert('The search returned no results, please try again.')
+}
+}
+); */
+
+//Acá quedaba armado pero me sobrescribia 
+
+
+
+/* let filtrados = traerDescripcion.concat(traerNombre);
+// console.log(filtrados);
+// Acá unifico los 2 arrays generados para armar un unico repetidor
+
+let mostrarFiltrados = "";
+
+for (let filtro of filtrados) {
+mostrarFiltrados += traerTarjeta(filtro);
+}
+if (filtrados.length > 0) {
+document.getElementById('tarjeta').innerHTML = mostrarFiltrados;
+}
+else alert('The search returned no results, please try again.');*/
  
 
+//Esto funcionaba a medias, si bien achicaba todo a un solo for, me traia mas de una tarjeta al estar concateando a un solo array
--- JS/past.js
+++ JS/past.js
@@ -5,12 +5,47 @@ let currentDate = new Date(data.currentDate);
 
 for(let event of data.events){
 
-    let eventDate = new Date(event.date);
+let eventDate = new Date(event.date);
 
-    if (eventDate < currentDate) {
-        tarjetasPasadas += traerTarjeta(event);
-    }
+if (eventDate < currentDate) {
+    tarjetasPasadas += traerTarjeta(event);
+}
 }
 
 let tarjeta = document.getElementById('tarjeta');
 tarjeta.innerHTML = tarjetasPasadas;
+
+//Pasar search y procesarlo
+
+let search = document.querySelector('form');
+search.addEventListener('submit', e => {
+    e.preventDefault();
+    let texto = document.querySelector('.form-control').value.trim().toLowerCase();
+    let filtrados = data.events.filter(e => {
+        return e.name.toLowerCase().includes(texto) || e.description.toLowerCase().includes(texto);
+    });
+
+    let currentDate = new Date(data.currentDate);
+
+    let filtradosPasados = "";
+    
+    for(let event of filtrados){
+      let eventDate = new Date(event.date);
+  
+      if (eventDate < currentDate) {
+        filtradosPasados += traerTarjeta(event);
+        document.getElementById('tarjeta').innerHTML = filtradosPasados
+      } 
+  
+    else {
+        alert('The search returned no results, please try again.');
+    }//Acá tengo que ver porque me tira el alert el numero de veces correspondiente a las tarjetas
+}
+  });
+    
+
+
+  
+
+
+
